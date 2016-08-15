6a692a44180dae3fd0f2cb7495dfe30a57f1e0ec
Gary McGhee <gary@buzzware.com.au>
2016-08-15 16:35:22 +0800

    following https://www.codementor.io/javascript/tutorial/es2015-project-setup-browserify-babel-hot-reloading

---
+++ index.js
@@ -0,0 +1,6 @@
+// people.js
+const fullNames = ['Drew Minns', 'Heather Payne', 'Kristen Spencer', 'Wes Bos', 'Ryan Christiani'];
+
+const firstNames = fullNames.map(name => name.split(' ').shift());
+
+export default firstNames; // ["Drew", "Heather", "Kristen", "Wes", "Ryan"]
